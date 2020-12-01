d2d5bc1914c712dc4b31653dcbf3a139566526b1
sundstromanders <anders.sundstrom@aalto.fi>
2020-12-01 15:10:30 +0200

    both parts of day 1 done

--- 1/solver.c
+++ 1/solver.c
@@ -21,18 +21,40 @@ int *parser(int *A) {
   return A;
 }
 
-int main() {
-
-  int A[MAX_ARRAY_SIZE];
-  parser(A);
+// Solves the first problem of the first day
+void solve_part_1(int A[]) { 
   for (int i = 0; i < MAX_ARRAY_SIZE - 1; i++) {
     for (int j = i + 1; j < MAX_ARRAY_SIZE; j++) {
       if (A[i] + A[j] == 2020) {
-	printf("result: %d\n", A[i] * A[j]);
-	return 0;	
+	printf("The result for part 1: %d\n", A[i] * A[j]);
+	return;	
       }
     }
   }
-  printf("Result not found!\n");
-  return -1;
+  printf("Result not found! (part 1)\n");
+}
+
+// Solves the second problem of the first day
+void solve_part_2(int A[]) {
+  for (int i = 0; i < MAX_ARRAY_SIZE - 2; i++) {
+    for (int j = i + 1; j < MAX_ARRAY_SIZE - 1; j++) {
+      for (int k = j + 1; k < MAX_ARRAY_SIZE; k++) {
+	if (A[i] + A[j] + A[k] == 2020) {
+	  printf("The result for part 2: %d\n", A[i] * A[j] * A[k]);
+	  return;
+	}
+      }
+    }
+  }
+  printf("Result not found! (part 2)\n");
+}
+
+int main() {
+  int A[MAX_ARRAY_SIZE];
+  parser(A);
+
+  solve_part_1(A);
+  solve_part_2(A);
+
+  return 0;
 }
